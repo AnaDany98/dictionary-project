17a49f52968a6b6fc9adb414d97b7a53079f8e83
AnaDany98 <105895422+AnaDany98@users.noreply.github.com>
2025-05-26 13:35:04 -0400

    Added the footer

--- src/App.js
+++ src/App.js
@@ -3,8 +3,38 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <h1>Hello world</h1>
+      <div className="container">
+        <header className="App-header"></header>
+        <h1>Hello world</h1>
+      </div>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/ana-daniela-garc%C3%ADa-lara-001336216/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Ana Daniela Garcia Lara
+        </a>{" "}
+        and is
+        <a
+          href="https://github.com/AnaDany98/dictionary-project"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>{" "}
+        and
+        <a
+          href="https://eclectic-syrniki-f10d7b.netlify.app//"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          {" "}
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
