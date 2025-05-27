159a51999fc416b8878606bd96ecd0fe797d806f
AnaDany98 <105895422+AnaDany98@users.noreply.github.com>
2025-05-27 11:12:51 -0400

    Added api documentation url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -10,11 +10,12 @@ export default function Dictionary(){
     }
 
     function search(event){
-        event.preventDefault();
+      event.preventDefault();
 
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+      //Documentation: https://dictionaryapi.dev/
+      let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 
-        axios.get(apiUrl).then(handleResponse);
+      axios.get(apiUrl).then(handleResponse);
     }
 
 
